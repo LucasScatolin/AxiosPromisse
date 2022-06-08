037a67b1f578f0e497a1e6b51450e2ee436d5c71
Scatolin, Lucas <lucas@madeincode.com.br>
2022-06-08 09:28:47 -0300

    Inicial commit teste

---
+++ main.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const gitURL = 'https://api.github.com/users/LucasScatolin'
+
+class ApiAxios { 
+    static async getDateGitHub(urlProfileApi)
+    {
+      try{
+          const response = new filterApi((await axios.get(urlProfileApi)).data)
+          console.log(response);
+      }
+      catch(e) { e => console.log(e)}; 
+
+    }   
+}
+
+
+class filterApi{
+    
+    constructor({ login, id, url}){
+        this.id = id,
+        this.login = login,
+        this.url = url
+    }
+}
+  
+
+ApiAxios.getDateGitHub(gitURL);
